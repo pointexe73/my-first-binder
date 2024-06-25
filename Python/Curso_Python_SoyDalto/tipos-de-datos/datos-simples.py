eeac64f162b2df1e2fc061d8159890bcbd53b825
pointexe73 <mejluis98@gmail.com>
2024-06-25 11:02:00 -0400

    Cambio-de-nombre

---
+++ Python/Curso_Python_SoyDalto/tipos-de-datos/datos-simples.py
@@ -0,0 +1,22 @@
+# string (str)
+"string"
+'string'
+
+"""tus datos son:
+        nombre: lucas
+        apellido: dalto"""
+        
+'''tus datos son:
+        nombre: lucas
+        apellido: dalto'''
+        
+
+# enteros (int)
+40
+
+# flotantes (float)
+40.2
+
+# booleanos (bool)
+True
+False
