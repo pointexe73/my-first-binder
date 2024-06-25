25758bd63bc01636d570cf3cf5a43f414d175c68
pointexe73 <mejluis98@gmail.com>
2024-06-25 11:46:24 -0400

    Variables

---
+++ Python/Curso_Python_SoyDalto/variables/variables.py
@@ -0,0 +1,16 @@
+
+# Definiendo una variable con camelCase
+nombreCompletoDeTuTioMaster = "Lucas Dalto"
+# Definiendo una variable con snake_case
+nombre_completo_de_tu_tio_master = "Lucas Dalto"
+
+# Concatenar con +
+bienvenida = "Hola " + nombreCompletoDeTuTioMaster + " ¿Como estas?"
+
+# Concatenar con f-strings
+bienvenida = f"Hola {nombre_completo_de_tu_tio_master} ¿Como estas?"
+
+# Operadores de pertenencia (in, not in)
+
+print("lucas" in bienvenida) # True
+print("lucas" not in bienvenida) # False
