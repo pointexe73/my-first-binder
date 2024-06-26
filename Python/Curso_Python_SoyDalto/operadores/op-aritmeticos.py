a443b45b400230b270701241ff1c09fbce92dc48
pointexe73 <mejluis98@gmail.com>
2024-06-26 11:48:26 -0400

    Update

--- Python/Curso_Python_SoyDalto/operadores/op-aritmeticos.py
+++ Python/Curso_Python_SoyDalto/operadores/op-aritmeticos.py
@@ -1,1 +1,24 @@
-Minuto_del_curso = 1,13,36
+# suma y resta (+ y -)
+suma = 12 + 5
+resta = 12 - 5
+
+# multiplicacion y division (* y /)
+multiplicacion = 12 * 5
+division = 12 / 5
+
+# potenciacion (**)
+exponente = 12 ** 5
+
+# division entera (//)
+division_entera = 12 // 5
+
+# resto o modulo (%)
+resto = 12 % 5
+
+print(f"Suma: {suma}")
+print(f"Resta: {resta}")
+print(f"Multiplicacion: {multiplicacion}") 
+print(f"Division: {division}")
+print(f"Exponente: {exponente}")
+print(f"Division Entera: {division_entera}")
+print(f"Resto: {resto}")
