7795496378fd06125a23fb8714bbb23c4ea61eb4
pointexe73 <mejluis98@gmail.com>
2024-06-26 23:05:11 -0400

    Update

--- Python/Curso_Python_SoyDalto/operadores/op-comparacion.py
+++ Python/Curso_Python_SoyDalto/operadores/op-comparacion.py
@@ -1,2 +1,36 @@
-# OPERADORES DE COMPARACION
- 
+#OPERADORES DE COMPARACION
+
+#igualdad (==)
+igual = 5 == 6
+
+#diferencia (!=)
+diferente = 5 != 6
+
+#mayor (>)
+mayor = 5 > 6
+
+#menor (<)
+menor = 5 < 6
+
+#mayor o igual (>=)
+mayor_o_igual = 5 >= 6
+
+#menor o igual (<=)
+menor_o_igual = 5 <= 6
+
+# calculos combinados
+
+a = 5 
+b = 10
+c = 20
+comparacion = a + b < c
+
+print(f"igual: {igual}")
+print(f"diferente: {diferente}")
+print(f"mayor: {mayor}")
+print(f"menor: {menor}")
+print(f"mayor_o_igual: {mayor_o_igual}")
+print(f"menor_o_igual: {menor_o_igual}")
+
+# calculos combinados
+print(f"comparacion: {comparacion}")
