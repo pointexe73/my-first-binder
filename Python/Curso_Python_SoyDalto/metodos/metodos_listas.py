04769c3df7a50223f9a22aa93911489cade3fadd
pointexe73 <mejluis98@gmail.com>
2024-07-03 13:36:52 -0400

    1:36-3/7-2024

--- Python/Curso_Python_SoyDalto/metodos/metodos_listas.py
+++ Python/Curso_Python_SoyDalto/metodos/metodos_listas.py
@@ -2,28 +2,44 @@
 
 # (-- LIST --)
 # creando una lista con list()
-lista = list(["Lucas Dalto", "Soy Dalto", True, 185])
+lista = ["Lucas Dalto", "Soy Dalto"]
 
 # (-- LEN --) devuelve la cantidad de elementos de una lista
 cantidad_elementos = len(lista)
 
 # (-- APPEND --) agrega un elemento al final de la lista
-agrega_elemento = lista.append("jajaja")
+lista.append("jajaja")
 
 # (-- INSERT --) agrega un elemento en una posicion determinada
-inserta_elemento = lista.insert(2, "test")
+lista.insert(2, "test")
 
 # (-- EXTENT --) agrega varios elementos al final de la lista
-extiende_lista = lista.extend(["test2", "test3"]) # se agrega en forma de lista
+lista.extend(["test2", "test3"])  # se agrega en forma de lista
 
+# (-- POP --) elimina un elemento de la lista
+lista.pop(0)  # para eliminar el primer elemento (-1)
 
+# (-- REMOVE --) elimina el primer elemento que se encuentre
+lista.remove("jajaja")  # elimina el valor por su nombre
 
+# (-- CLEAR --) elimina todos los elementos de la lista
+# lista.clear() # se comenta la linea para no eliminar la lista
 
-print(f"Lista '--List--': {lista}")
-print(f"Cantidad de elementos '--Len--': {cantidad_elementos}")
-print(f"Agregando un elemento '--Append--': {agrega_elemento}")
-print(f"Insertando un elemento en una posicion determinada '--Insert--': {inserta_elemento}")
-pritn(f"Extendiendo una lista '--Extend--': {extiende_lista}")
+# (-- SORT --) ordena los elementos de la lista
+lista.sort()  # ordena de menor a mayor
+# lista.sort(reverse=True) # ordena de mayor a menor
 
+# (-- REVERSE --) invierte el orden de los elementos de la lista
+lista.reverse()
 
-2.17.41
+print(f"'--List--'    Lista                  : {lista}")
+print(f"'--Len--'     Cantidad de elementos  : {cantidad_elementos}")
+print(f"'--Append--'  Agregando un elemento  : {lista[-1]}")
+print(f"'--Insert--'  Insertando un elemento : {lista[2]}")
+print(f"'--Extend--'  Extendiendo una lista  : {lista[-2:]}")
+print(f"'--Pop--'     Eliminando un elemento : {lista[0]}")
+print(f"'--Remove--'  Eliminando un elemento : {lista[1]}")
+print(f"'--Clear--'   Limpando la lista      : {lista}")
+print(f"'--Sort--'    Ordenando la lista     : {lista}")
+print(f"'--Reverse--' Revertiendo la lista   : {lista[::-1]}")
+print(lista)
