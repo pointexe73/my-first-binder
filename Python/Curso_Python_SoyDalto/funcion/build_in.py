d1ba49923c19e2d07e0fa09dc8e522eb78b8d749
pointexe73 <mejluis98@gmail.com>
2024-07-31 17:50:08 -0400

    31.07.2024

---
+++ Python/Curso_Python_SoyDalto/funcion/build_in.py
@@ -0,0 +1,14 @@
+# FUNCIONES
+
+# Lista de números
+numeros = [4, 7, 1, 42, 15]
+
+# Encontrar el número más alto en la lista
+numero_mas_alto = max(numeros)
+# Imprimir el número más alto
+print(f'El número más alto es: {numero_mas_alto}')
+
+# Encontrar el número más bajo en la lista
+numero_mas_bajo = min(numeros)
+# Imprimir el número más bajo
+print(f'El número más bajo es: {numero_mas_bajo}')
